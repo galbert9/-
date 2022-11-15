bb74ea808315eb2e9351e6140b6943a503fdc700
Gennady <galbert9@gmail.com>
2022-11-15 16:41:25 +0300

    разраяд числа через логарифм

---
+++ #3 Разряд числа с помощью логарифма/Program.cs
@@ -0,0 +1,15 @@
+﻿// Определить разряд числа с прмощью логарифма
+
+Console.WriteLine("Введите число");  
+int n = Convert.ToInt32 (Console.ReadLine());
+
+double exp = Math.Log10(n);
+
+int digit = 0;
+while (digit < exp)
+{
+    if (digit > exp) break;
+    digit++;
+    
+}
+Console.WriteLine(digit);
