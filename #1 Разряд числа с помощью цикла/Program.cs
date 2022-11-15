bee21cd19f15920dc847224680695cd2a5f14471
Gennady <galbert9@gmail.com>
2022-11-15 12:56:59 +0300

    Разряд числа через цикл

---
+++ #1 Разряд числа с помощью цикла/Program.cs
@@ -0,0 +1,12 @@
+﻿// Разряд числа с помощью цикла
+ 
+Console.WriteLine("Введите число");  
+int n = Convert.ToInt32 (Console.ReadLine());
+int digit = 0;
+while (n % 10 > 0)
+{
+    digit++;
+    n/=10;
+}
+Console.WriteLine(digit);
+Console.WriteLine("");
